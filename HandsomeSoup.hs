ea8e085ab31b1b17ef101038a840a0b3a41e4de4
Aditya Bhargava <batman@scribd.com>
2012-04-22 16:29:13 -0700

    use list instead of map for attrs

--- HandsomeSoup.hs
+++ HandsomeSoup.hs
@@ -13,57 +13,61 @@ import Data.Monoid (mconcat)
 import qualified Data.Functor.Identity as I
 import qualified Debug.Trace as D
 
-data Selector = Selector { sName :: String, sAttrs :: M.Map String String }
+-- if no tag name was given, sName will be set to '*'
+
+data Selector = Selector { sName :: String, sAttrs :: [(String,String)] }
 
 instance Show Selector where
   show (Selector name attrs) = show name ++ ":" ++ showMap attrs
-      where showMap m = (init.init $ "{" ++ (foldl (\acc (k,v) -> acc ++ (show k) ++ ":" ++ (show v) ++ ", ") "" (M.assocs m))) ++ "}"
+      where showMap m = (init.init $ "{" ++ (foldl (\acc (k,v) -> acc ++ (show k) ++ ":" ++ (show v) ++ ", ") "" m)) ++ "}"
 
 combinator = char ' ' <|> char '+' <|> char '>' <|> char ','
 
---| selects a tag name, like "h1"
+-- | selects a tag name, like "h1"
 tag :: ParsecT [Char] u I.Identity [Char]
 tag = many1 alphaNum
 
--- secondarySelector = char ':' >> many1 (alphaNum <|> oneOf "-")
+-- | selects a pseudo class, like ":link", ":first-child" etc.
+pseudoClass :: ParsecT [Char] u I.Identity [Char]
+pseudoClass = char ':' >> many1 (alphaNum <|> oneOf "-")
 
 -- tagAttribute = between (char '[') (char ']') (many1 alphaNum)
 
---| class selector, selects ".foo"
+-- | class selector, selects ".foo"
 klass :: ParsecT [Char] u I.Identity ([Char], [Char])
 klass = do
     char '.' 
     val <- many1 alphaNum
     return ("class", val)
 
---| id selector, selects "#foo"
+-- | id selector, selects "#foo"
 id_ :: ParsecT [Char] u I.Identity ([Char], [Char])
 id_ = do
     char '#'
     val <- many1 alphaNum
     return ("id", val)
 
---| universal selector, selects "*"
+-- | universal selector, selects "*"
 universalSelector :: ParsecT [Char] u I.Identity String
 universalSelector = string "*"
 
---| selects a tagname followed by one or more secondary selectors
---| example: "a.foo", "*#hello", "h1" etc
+-- | selects a tagname followed by one or more secondary selectors
+-- | example: "a.foo", "*#hello", "h1" etc
 tagSelector :: ParsecT [Char] u I.Identity Selector
 tagSelector = do
     tagName <- tag <|> universalSelector
     attrs <- many1 (klass <|> id_) <|> (return [])
-    return $ Selector tagName (M.fromList attrs)
+    return $ Selector tagName attrs
 
 secondarySelector = do
     attrs <- many1 (klass <|> id_)
-    return $ Selector "*" (M.fromList attrs)
+    return $ Selector "*" attrs
 
---| A simple selector is either a type selector or universal selector followed immediately by zero or more attribute selectors, ID selectors, or pseudo-classes, in any order.
+-- | A simple selector is either a type selector or universal selector followed immediately by zero or more attribute selectors, ID selectors, or pseudo-classes, in any order.
 simpleSelector :: ParsecT [Char] u I.Identity Selector
 simpleSelector = tagSelector <|> secondarySelector
 
---| One or more simple selectors separated by combinators
+-- | One or more simple selectors separated by combinators
 selector :: ParsecT [Char] u I.Identity [[Selector]]
 selector = many1 simpleSelector `sepBy` (spaces >> combinator >> spaces)
 
@@ -77,8 +81,8 @@ css tag = case (parse selector "" tag) of
 -- TODO remember to account for "*"! The universal selector.
 fromSelectors (s:selectors) = foldl (\acc selector -> acc <+> make selector) (make s) selectors
   where make (Selector name attrs) 
-          | name == "*" = multi this >>> makeAttrs (M.toList attrs)
-          | otherwise = multi $ hasName name >>> makeAttrs (M.toList attrs)
+          | name == "*" = multi this >>> makeAttrs attrs
+          | otherwise = multi $ hasName name >>> makeAttrs attrs
         makeAttrs (a:attrs) = foldl (\acc attr -> acc >>> makeAttr attr) (makeAttr a) attrs
         makeAttrs [] = this
         makeAttr (name, value) = hasAttrValue name (==value)
