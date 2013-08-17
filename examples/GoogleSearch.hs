e32b8aa065abaaa6f5e5327a62045ce922831b66
Aditya Bhargava <bluemangroupie@gmail.com>
2013-08-16 20:21:02 -0700

    updating example and readme

--- examples/GoogleSearch.hs
+++ examples/GoogleSearch.hs
@@ -2,8 +2,10 @@ module GoogleSearch where
 import Text.HandsomeSoup
 import Text.XML.HXT.Core
 import Control.Monad
+import Control.Monad.IO.Class
 
 main = do
-    doc <- fromUrl "http://www.google.com/search?q=egon+schiele"
+    doc <- liftIO $ fromUrl "http://www.google.com/search?q=egon+schiele"
     links <- runX $ doc >>> css "h3.r a" ! "href"
     mapM_ putStrLn links
+
