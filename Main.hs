c5fd6fb0353d93ab100fee62f6e3c3891ac9986c
Aditya Bhargava <bluemangroupie@gmail.com>
2014-04-15 17:20:21 -0700

    adding a test file

---
+++ Main.hs
@@ -0,0 +1,9 @@
+import Text.HandsomeSoup
+import Text.XML.HXT.Core
+import Control.Monad
+import Control.Arrow
+
+main = do
+    let doc = fromUrl "http://www.google.com/search?q=egon+schiele"
+    links <- runX $ doc >>> css "h3.r a" ! "href"
+    mapM_ putStrLn links
