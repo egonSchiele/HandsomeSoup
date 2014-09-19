dffb4358436a6fdd2cd9e31c368f937d618b4778
timmy_tofu <timmytofu@users.noreply.github.com>
2014-09-19 18:34:06 -0400

    Changes executable to one in example folder, builds executable (example) only when buildExample flag is set, has executable depend on library, removes redundant dependencies

--- Main.hs
+++
@@ -1,9 +0,0 @@
-import Text.HandsomeSoup
-import Text.XML.HXT.Core
-import Control.Monad
-import Control.Arrow
-
-main = do
-    let doc = fromUrl "http://www.google.com/search?q=egon+schiele"
-    links <- runX $ doc >>> css "h3.r a" ! "href"
-    mapM_ putStrLn links
